4885b1223be2a715028b444fe06ff4a10a149663
Neo Ren <bitaimov@gmail.com>
2024-06-22 10:05:29 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by cduoyue on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
