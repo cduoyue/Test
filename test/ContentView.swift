960283f0478a1423ba442904866a0cdd28153af3
cduoyue <156039680+cduoyue@users.noreply.github.com>
2024-06-22 11:29:12 +0800

    Update ContentView.swift

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Swift!")
+            Text("Hello, Swift b4!")
         }
         .padding()
     }
